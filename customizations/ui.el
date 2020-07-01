1dbbed299b44adcfff2447c469205259fdeae0cd
davidvujic <david@vujic.se>
2020-07-01 17:23:20 +0200

    fix: replace deprecated var

--- customizations/ui.el
+++ customizations/ui.el
@@ -38,10 +38,10 @@
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
-      x-select-enable-clipboard t
+      select-enable-clipboard t
 
       ;; I'm actually not sure what this does but it's recommended?
-      x-select-enable-primary t
+      select-enable-primary t
 
       ;; Save clipboard strings into kill ring before replacing them.
       ;; When one selects something in another program to paste it into Emacs,
@@ -71,7 +71,7 @@
 (when (eq system-type 'darwin)
   (setq mac-option-modifier nil
       mac-command-modifier 'meta
-      x-select-enable-clipboard t))
+      select-enable-clipboard t))
 
 (setq-default cursor-type 'bar)
 
