5b4ce629c70047e2eb63af0591177f8b2ed2d84a
David Vujic <david@vujic.se>
2020-11-21 14:20:06 +0100

    fix: python auto complete

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -13,16 +13,17 @@
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (defun setup-python-mode ()
+  "Python mode setup."
   (require 'pyenv-mode-auto)
   (setq gud-pdb-command-name "python -m pdb")
-  (setq jedi:complete-on-dot t)
-  (setq jedi:get-in-function-call-delay 0)
   (add-to-list 'company-backends 'company-jedi)
   (jedi-mode)
-  (pipenv-mode +1))
+  (pipenv-mode +1)
+  (company-mode +1))
 
 (add-hook 'python-mode-hook 'setup-python-mode)
 
+
 (provide 'setup-python)
 
-;;; setup-python.el ends heres
+;;; setup-python.el ends here
