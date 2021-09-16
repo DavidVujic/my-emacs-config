8fd6c3f0766d53eaca4b68a9792c1020e7a80731
davidvujic <david@vujic.se>
2021-09-16 17:23:00 +0200

    Use ipython, partially to sidestep the annoying 'missing readline' warnings when activating a python shell

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -8,8 +8,8 @@
 
 (setq elpy-rpc-python-command "python3")
 
-(setq python-shell-interpreter "python3"
-      python-shell-interpreter-args "-i")
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
 
 (elpy-enable)
 
