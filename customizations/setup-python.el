17cf5e0180c6d60d7a1a94412a4aa5e5be633b93
David Vujic <david@vujic.se>
2021-10-06 12:02:00 +0200

    Define polymode: Python with SQL inline syntax

--- customizations/setup-python.el
+++ customizations/setup-python.el
@@ -20,6 +20,20 @@
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
+;; Polymode: syntax highlighting for inline SQL statements in Python
+(define-hostmode poly-python-hostmode :mode 'python-mode)
+
+(define-innermode poly-sql-expr-python-innermode
+    :mode 'sql-mode
+    :head-matcher (rx (= 3 (char "\"'")) (zero-or-more (any "\t\n ")) (or "SELECT" "INSERT" "UPDATE" "DELETE"))
+    :tail-matcher (rx (= 3 (char "\"'")))
+    :head-mode 'host
+    :tail-mode 'host)
+
+(define-polymode poly-python-sql-mode
+    :hostmode 'poly-python-hostmode
+    :innermodes '(poly-sql-expr-python-innermode))
+
 (defun setup-python-mode ()
   "Python mode setup."
   (require 'pyenv-mode-auto)
