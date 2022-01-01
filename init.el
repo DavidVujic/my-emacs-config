162f5db77f817593e2b23c1f1bc19884ab21f296
davidvujic <david@vujic.se>
2022-01-01 14:57:22 +0100

    fix: remove neotree font face settings

--- init.el
+++ init.el
@@ -102,14 +102,7 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(package-selected-packages (quote (spinner sesman queue parseedn))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(neo-dir-link-face ((t (:height 125))))
- '(neo-file-link-face ((t (:height 125 :foreground "silver"))))
- '(neo-root-dir-face ((t (:height 125)))))
+
 
 (provide 'init)
 
