07beb0f1c435b8673824ac5e03ffdc08efafb9c7
davidvujic <david@vujic.se>
2023-08-06 17:33:01 +0200

    refactor: load config on init

--- init.el
+++ init.el
@@ -96,23 +96,7 @@
 (load "setup-yaml.el")
 (load "setup-docker.el")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(custom-safe-themes
-   '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default))
- '(package-selected-packages '(spinner sesman queue parseedn)))
-
 
 (provide 'init)
 
 ;;; init.el ends here
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
