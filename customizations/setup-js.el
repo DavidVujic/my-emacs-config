9ded70be9dcbb006a26432d1998f5d1e70ff41da
David Vujic <david@vujic.se>
2023-10-05 09:21:27 +0200

    fix(ts): run js mode for .ts files

--- customizations/setup-js.el
+++ customizations/setup-js.el
@@ -9,7 +9,7 @@
 ;; add local node modules to path
 (add-hook 'flycheck-mode-hook 'add-node-modules-path)
 
-(add-to-list 'auto-mode-alist '("\\.[c|m]?js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.[c|m]?[j|t]s\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 
