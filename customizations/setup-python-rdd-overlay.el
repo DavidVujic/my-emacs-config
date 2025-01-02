e5869c4ed5669cddb79caae5cb2b72eec9933c2f
David Vujic <david@vujic.se>
2025-01-02 17:40:04 +0100

    fix(rdd overlay): apply python-mode syntax highlighting to the RDD overlay

--- customizations/setup-python-rdd-overlay.el
+++ customizations/setup-python-rdd-overlay.el
@@ -15,16 +15,22 @@
 (defvar rdd-py/overlay-content-max-length 400
   "Max length for content in overlay.")
 
-(defun rdd-py/overlay-face ()
-  "The styling of the contents in an overlay."
-  '(:foreground "green"
-               :background "grey10"
-               :box (:line-width -1 :color "black" :alpha 0.5)
-               :weight normal))
+(defun rdd-py/multiline? (content)
+  "Check if CONTENT is multiline or not."
+  (string-match-p "\n" content))
+
+(defun rdd-py/handle-possible-multiline-content (result)
+  "Apply formatting to the RESULT, handling single and multiline content."
+  (if (rdd-py/multiline? result)
+      (concat "\n" result "\n")
+    result))
 
 (defun rdd-py/overlay-styling (result)
   "The styling of the RESULT."
-  (propertize (format " => %s" result) 'face (rdd-py/overlay-face)))
+  (let ((content (rdd-py/handle-possible-multiline-content result) ))
+    (propertize (format " => %s" content)
+                'line-prefix "    "
+                'face 'default)))
 
 (defun rdd-py/truncate-content (result)
   "Truncate RESULT if longer than a configured limit."
@@ -42,25 +48,36 @@
 (defun rdd-py/check-cursor-movement ()
   "Check if the cursor has moved, and remove the overlay if it has."
   (when
-    (not (equal (point) rdd-py/my-last-cursor-pos)) ; Check if cursor moved
+    (not (equal (point) rdd-py/my-last-cursor-pos))
     (rdd-py/remove-existing-overlay)
     (remove-hook 'post-command-hook #'rdd-py/check-cursor-movement)))
+
+(defun rdd-py/format-content-with-mode (content)
+  "Return CONTENT formatted as \"python-mode\"."
+  (with-temp-buffer
+    (insert content)
+    (funcall 'python-mode)
+    (setq font-lock-mode t)
+    (font-lock-fontify-region (point-min) (point-max))
+    (buffer-substring (point-min) (point-max))))
+
+(defun rdd-py/format-output (output)
+  "Apply formatting and truncating to OUTPUT."
+  (let* ((styled-output (rdd-py/overlay-styling output))
+         (python-styled-output (rdd-py/format-content-with-mode styled-output))
+         (truncated-output (rdd-py/truncate-content python-styled-output)))
+    truncated-output))
 
 (defun rdd-py/output-overlay (output)
    "Show overlay for evaluated OUTPUT."
   (rdd-py/remove-existing-overlay)
-  ;; Calculate the position after the last statement
   (let ((line-end (save-excursion
                     (move-end-of-line 1)
-                    (skip-chars-backward " \t") ; Ignore trailing spaces/tabs
+                    (skip-chars-backward " \t")
                     (point))))
-    ;; Create a new overlay at the calculated position
     (setq rdd-py/overlay (make-overlay line-end line-end))
-    (overlay-put rdd-py/overlay 'after-string
-                 (rdd-py/overlay-styling (rdd-py/truncate-content output))))
-  ;; Record the current cursor position
+    (overlay-put rdd-py/overlay 'after-string (rdd-py/format-output output)))
   (setq rdd-py/my-last-cursor-pos (point))
-  ;; Add a hook to check for cursor movement
   (add-hook 'post-command-hook #'rdd-py/check-cursor-movement))
 
 ;;; setup-python-rdd-overlay.el ends here
